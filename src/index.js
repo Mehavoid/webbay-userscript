c6fb4ae8946d730af5e38dd4d61087b16615aabd
Mehavoid <tontole@riseup.net>
2022-09-22 20:40:17 +0300

    Refactor script
    - Add `ok`, `fail` and `warn` handlers
    - Made `eventHandler` a short
    - Fix handle `request` exception

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@
 const DOMAIN = ['krakenfiles', 'webshare'];
 
 const request = (url, body) =>
-  fetch(url, {
-    method: 'POST',
-    body,
-  }).then((res) => res.json());
+  fetch(url, { method: 'POST', body }).then((res) => {
+    const { status } = res;
+    if (status === 200) return res.json();
+    throw new Error('HTTP status code: ${status}');
+  });
 
 const api = (data) => {
   const url = 'http://127.0.0.1:8787/api/link';
@@ -24,31 +25,41 @@ const redirect = (url) => {
 
 const setPrivacy = (a) => a.setAttribute('rel', 'noopener noreferrer');
 
-const match = (url, whitelist) => {
-  const chunks = url.split('.');
+const match = ({ host }) => {
+  const chunks = host.split('.');
   while (chunks.length > 0) {
     const chunk = chunks.pop();
-    if (whitelist.includes(chunk)) return true;
+    if (DOMAIN.includes(chunk)) return true;
   }
   return false;
 };
 
+const ok = ({ success, data }) => {
+  if (success) redirect(data);
+  return success;
+};
+
+const fail = () => false;
+
+const warn = (target) => (success) => {
+  const span = document.createElement('span');
+  span.classList.add('wb', `wb-${success}`);
+  if (success) target.classList.add('wb-noclick');
+  target.appendChild(span);
+};
+
+const tags = {
+  A: match,
+};
+
 const eventHandler = (event) => {
   const { target } = event;
-  if (target.tagName !== 'A') return true;
-  const { href } = target;
-  const matched = match(href, DOMAIN);
-  if (!matched) return true;
+  const fn = tags[target.tagName];
+  if (!(fn && fn(target))) return false;
   console.info({ event });
-  return api({ link: href }).then(({ success, data }) => {
-    if (success) {
-      event.preventDefault();
-      redirect(data);
-    } else {
-      setPrivacy(target);
-    }
-    return false;
-  });
+  setPrivacy(target);
+  event.preventDefault();
+  return api({ link: target.href }).then(ok, fail).then(warn(target));
 };
 
 document.addEventListener('click', eventHandler, false);
